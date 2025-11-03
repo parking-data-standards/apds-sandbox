d890637416d50a542a72282165aa70d7f33c742a
Markus Schneider <markus.schneider@plexx-digital.com>
2025-11-03 09:00:47 +0100

    prepare Swagger view

--- apiv4/swagger-initializer.js
+++ apiv4/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://sandbox.parking-data-standard.org/apiv4/apds4.yaml",
+    url: "https://sandbox.parking-data-standards.org/apiv4/apds4.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
